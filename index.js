0393172d4720a7df8536937302979f8c33c900a8
ktokto313 <57142191+ktokto313@users.noreply.github.com>
2022-04-28 22:01:46 +0700

    commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var currentSlide = 0;
 
 function changeSlide(direction) {
-    console.log("run");
     if (direction == 1) {
         getSlide(currentSlide).style.display = "none";
         getSlide(++currentSlide).style.display = "block";
@@ -14,16 +13,13 @@ function changeSlide(direction) {
 function getSlide(n) {
     array = document.getElementsByClassName("slide");
     if (!array.length > 1) {
-        console.log("not long enough");
         return array[0];
     }
     if (n == array.length) {
-        console.log("overflow");
         currentSlide = 0
         return array[currentSlide];
     }
     if (n == -1) {
-        console.log("underflow");
         currentSlide = array.length-1
         return array[currentSlide];
     }
